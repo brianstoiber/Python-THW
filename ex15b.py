d65aba0801d4abb6034cc6dd0e674be4401c203c
brianstoiber <brian@brianstoiber.com>
2015-02-26 15:00:54 -0600

    modify ex15.py

--- ex15b.py
+++ ex15b.py
@@ -1,9 +1,13 @@
+# Asks for the filename you want to open
 print "Type the filename you are looking for."
 filename = raw_input("> ")
 
+# Passes filename as txt
 txt = open(filename)
 
+# Prints Line 8 and then prints file
 print "Here's your file %r:" % filename
 print txt.read()
 
+# Closes the file
 txt.close()
